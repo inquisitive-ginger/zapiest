94ffd1bd2a03bdac47cc317528a3404bb4e1d743
Zach Pritchett <pritchett.za@gmail.com>
2018-05-15 16:17:04 -0700

    adding zap that converts a pocket entry to .pdf and uploads it to Google Drive

--- zaps/pocket_to_drive.py
+++ zaps/pocket_to_drive.py
@@ -1,12 +1,25 @@
-import sys
-sys.path.append('../api/')
+# Path hack.
+import sys, os
+sys.path.insert(0, os.path.abspath('..'))
 
-from Pocket import Pocket
-from CloudConvert import CloudConvert
-from GoogleDrive import GoogleDrive
+from api.CloudConvert import CloudConvert
+from api.GoogleDrive import GoogleDrive
+from api.Pocket import Pocket
 
-pocket_service = Pocket()
-cc_service = CloudConvert()
-drive_service = GoogleDrive()
+pocket_instance = Pocket()
+cc_instance = CloudConvert()
+drive_instance = GoogleDrive()
 
-pockets = pocket_service.get_pockets()
+pockets = pocket_instance.get_pockets(tag='convert', recent=True)
+
+for pocket in pockets:
+    converted_file = cc_instance.convert_url_to_pdf(pocket['given_url'])
+    file_name = pocket['resolved_title']
+
+    file_to_upload = {
+        'full_path': '../data/{0}'.format(converted_file),
+        'name': file_name
+    }
+
+    drive_instance.upload_to_folder(file_to_upload, 'Unprocessed')
+    cc_instance.delete_file(file_to_upload['full_path'])
